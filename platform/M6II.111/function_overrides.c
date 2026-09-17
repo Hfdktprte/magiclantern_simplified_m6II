47d55927d89b0f4dad521e04598b6300343c4305
Hfdktprte <55281292+Hfdktprte@users.noreply.github.com>
2026-09-18 04:42:26 +0800

    M6II: add mlv_lite D8 compatibility exports

--- platform/M6II.111/function_overrides.c
+++ platform/M6II.111/function_overrides.c
@@ -5,20 +5,9 @@
  * Copyright (C) 2021 Magic Lantern Team
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the
- * Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor,
- * Boston, MA  02110-1301, USA.
+ * modify it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  */
 
 #include <dryos.h>
@@ -29,15 +18,10 @@
 #include <lens.h>
 #include <edmac.h>
 
-/*
- * Partition tables stuff. Got inlined in new generations, but this is a pretty standard one.
- * Struct copied from bootflags.c as there's no header to include right now.
- */
-
 struct chs_entry
 {
     uint8_t head;
-    uint8_t sector; //sector + cyl_msb
+    uint8_t sector;
     uint8_t cyl_lsb;
 }__attribute__((packed));
 
@@ -53,7 +37,7 @@ struct partition
 
 struct partition_table
 {
-    uint8_t  state; // 0x80 = bootable
+    uint8_t  state;
     uint8_t  start_head;
     uint16_t start_cylinder_sector;
     uint8_t  type;
@@ -71,17 +55,12 @@ void fsuDecodePartitionTable(void * partIn, struct partition_table * pTable){
     pTable->end_head   = part->end.head;
     pTable->sectors_before_partition = part->start_sector;
     pTable->sectors_in_partition     = part->size;
-
-    //tricky bits - TBD
     pTable->start_cylinder_sector = 0;
     pTable->end_cylinder_sector   = 0;
 }
 
-/** GUI **/
-//see comments in stub.S
 void gui_init_end(void){ }
 
-//Not sure if all args are uint.
 extern void gui_enqueue_message(uint32_t, uint32_t, uint32_t, uint32_t);
 void GUI_Control(int bgmt_code, int obj, int arg, int unknown){
     gui_enqueue_message(0, bgmt_code, obj, arg);
@@ -94,104 +73,59 @@ void LoadCalendarFromRTC(struct tm *tm)
 
 int get_task_info_by_id(int unknown_flag, int task_id, void *task_attr)
 {
-    // task_id is something like two u16s concatenated.  The flag argument,
-    // present on D45 but not on D678 allows controlling if the task info request
-    // uses the whole thing, or only the low half.
-    //
-    // ML calls with this set to 1, meaning task_id is used as is,
-    // if 0, the high half is masked out first.
-    //
-    // D678 doesn't have the 1 option, we use the low half as index
-    // to find the full value.
     struct task *task = first_task + (task_id & 0xffff);
     return _get_task_info_by_id(task->taskId, task_attr);
 }
 
-/** File I/O **/
-
-/**
- * _FIO_GetFileSize returns now 64bit size in form of struct.
- * This probably should be integrated into fio-ml for CONFIG_DIGIC_VIII
- */
 extern int _FIO_GetFileSize64(const char *, void *);
 int _FIO_GetFileSize(const char * filename, uint32_t * size){
     uint32_t size64[2];
     int code = _FIO_GetFileSize64(filename, &size64);
-    *size = size64[0]; //return "lower" part
+    *size = size64[0];
     return code;
 }
 
-/** WRONG: temporary overrides to get CONFIG_HELLO_WORLD working **/
+/* Legacy EDMAC entry points are not used by the M6II D8 mlv_lite backend. */
+void SetEDmac(unsigned int channel, void *address, struct edmac_info *ptr, int flags) { return; }
+void ConnectWriteEDmac(unsigned int channel, unsigned int where) { return; }
+void ConnectReadEDmac(unsigned int channel, unsigned int where) { return; }
+void StartEDmac(unsigned int channel, int flags) { return; }
+void AbortEDmac(unsigned int channel) { return; }
+void RegisterEDmacCompleteCBR(int channel, void (*cbr)(void*), void* cbr_ctx) { return; }
+void UnregisterEDmacCompleteCBR(int channel) { return; }
+void RegisterEDmacAbortCBR(int channel, void (*cbr)(void*), void* cbr_ctx) { return; }
+void UnregisterEDmacAbortCBR(int channel) { return; }
+void RegisterEDmacPopCBR(int channel, void (*cbr)(void*), void* cbr_ctx) { return; }
+void UnregisterEDmacPopCBR(int channel) { return; }
+void _EngDrvOut(uint32_t reg, uint32_t value) { return; }
 
-void SetEDmac(unsigned int channel, void *address, struct edmac_info *ptr, int flags)
-{
-    return;
-}
-
-void ConnectWriteEDmac(unsigned int channel, unsigned int where)
-{
-    return;
-}
-
-void ConnectReadEDmac(unsigned int channel, unsigned int where)
-{
-    return;
-}
-
-void StartEDmac(unsigned int channel, int flags)
-{
-    return;
-}
-
-void AbortEDmac(unsigned int channel)
-{
-    return;
-}
-
-void RegisterEDmacCompleteCBR(int channel, void (*cbr)(void*), void* cbr_ctx)
-{
-    return;
-}
-
-void UnregisterEDmacCompleteCBR(int channel)
-{
-    return;
-}
-
-void RegisterEDmacAbortCBR(int channel, void (*cbr)(void*), void* cbr_ctx)
-{
-    return;
-}
-
-void UnregisterEDmacAbortCBR(int channel)
-{
-    return;
-}
-
-void RegisterEDmacPopCBR(int channel, void (*cbr)(void*), void* cbr_ctx)
-{
-    return;
-}
-
-void UnregisterEDmacPopCBR(int channel)
-{
-    return;
-}
-
-void _EngDrvOut(uint32_t reg, uint32_t value)
-{
-    return;
-}
-
+/* D8 has no legacy shamem API. Keep unknown generic reads inert for the first
+ * native-14-bit mlv_lite bring-up; verified RAW redirection uses direct MMIO. */
 uint32_t shamem_read(uint32_t addr)
 {
     return 0;
 }
 
-void _engio_write(uint32_t* reg_list)
+/* Same compatibility exports used by Bilal's M50 port. SRM is intentionally
+ * disabled on M6II because Canon SRM allocation is known to crash this port. */
+struct memSuite * srm_malloc_suite(int num_requested_buffers)
+{
+    return 0;
+}
+
+void srm_free_suite(struct memSuite * suite)
 {
     return;
 }
+
+/* ErrCardForLVApp_handler does not exist on this D8 port; mlv_lite only needs
+ * an addressable compatibility symbol. */
+void ErrCardForLVApp_handler(void)
+{
+    return;
+}
+
+void _engio_write(uint32_t* reg_list) { return; }
 
 unsigned int UnLockEngineResources(struct LockEntry *lockEntry)
 {
