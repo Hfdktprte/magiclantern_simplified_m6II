403f29a812566a30a0cfb3b3c24053ec729e0e02
Hfdktprte <55281292+Hfdktprte@users.noreply.github.com>
2026-09-17 18:31:40 +0800

    M6II: simplify BOOTDISK disable script to validated universal call

--- tools/m6ii_bootflag/disable/extend.m
+++ tools/m6ii_bootflag/disable/extend.m
@@ -1,15 +1,6 @@
 ' EOS M6 Mark II BOOTDISK disabler / recovery
-' This reverses the persistent BOOTDISK camera flag set by enable/extend.m.
+' Reverses the persistent BOOTDISK camera flag.
 
 private sub Initialize()
-  System.Create()
   DisableBootDisk()
-
-  fileName = "A:/BOOTDISK_DISABLED.TXT"
-  RemoveFile(fileName)
-  f = OpenFileCREAT(fileName)
-  CloseFile(f)
-  f = OpenFileWR(fileName)
-  WriteFileString(f, "EOS M6 Mark II: DisableBootDisk() was called.\n")
-  CloseFile(f)
 end sub
