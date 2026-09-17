0e0ca3243e9830fa0d24bd0a275dc158f342e7af
Hfdktprte <55281292+Hfdktprte@users.noreply.github.com>
2026-09-17 18:31:25 +0800

    M6II: fix Canon Basic verifier SD drive path

--- tools/m6ii_bootflag/verify/extend.m
+++ tools/m6ii_bootflag/verify/extend.m
@@ -1,6 +1,7 @@
 ' EOS M6 Mark II read-only identification script
 ' This script does NOT modify BOOTDISK or camera NVRAM.
-' It is based on the maintained Canon Basic DIGIC 8 firmware-signature example.
+' Adapted from the maintained Canon Basic Universal/extend_fw_sign.m example.
+' EOS/DIGIC 8 Canon Basic uses B:/ for the SD card path.
 
 dim pRom0BaseAddress = 0xE0040000
 dim pRom1BaseAddress = 0xF0000000
@@ -17,22 +18,16 @@ private sub compute_signature(startSign, sLen)
 end sub
 
 private sub Initialize()
-  System.Create()
-
-  fileName = "A:/M6II_INFO.TXT"
+  fileName = "B:/M6II_INFO.TXT"
   RemoveFile(fileName)
+
   f = OpenFileCREAT(fileName)
   CloseFile(f)
-  f = OpenFileWR(fileName)
 
+  f = OpenFileWR(fileName)
   WriteFileString(f, "MODEL_ID=0x%08x\n", *pRom1BaseAddress)
   WriteFileString(f, "ROM_VERSION=%s\n", pRom1BaseAddress + 4)
   WriteFileString(f, "CANON_FW=%d\n", GetFirmwareVersion())
   WriteFileString(f, "ROM0_SIG_10000=0x%08x\n", compute_signature(pRom0BaseAddress, sigLen))
-
   CloseFile(f)
-
-  ' Also ask Canon firmware to emit its normal camera-information XML.
-  ' This call has been publicly demonstrated on EOS M6 Mark II Canon Basic.
-  CamInfo_Debug(1)
 end sub
