3eac6a3bf1a5c9560ac79c9152718c152d90e822
Hfdktprte <55281292+Hfdktprte@users.noreply.github.com>
2026-09-17 18:31:33 +0800

    M6II: simplify BOOTDISK enable script to validated universal call

--- tools/m6ii_bootflag/enable/extend.m
+++ tools/m6ii_bootflag/enable/extend.m
@@ -1,17 +1,8 @@
 ' EOS M6 Mark II BOOTDISK enabler
-' WARNING: this makes a persistent camera setting change.
-' Run the verify script first and continue only on the supported M6II.111 / 5.9.2 target.
-' Based on lclevy/cbasic_examples Universal/extend_bootdisk.m.
+' WARNING: persistent camera-side change.
+' Run only after the read-only verifier confirms the supported target.
+' Kept intentionally identical in behavior to Universal/extend_bootdisk.m.
 
 private sub Initialize()
-  System.Create()
   EnableBootDisk()
-
-  fileName = "A:/BOOTDISK_ENABLED.TXT"
-  RemoveFile(fileName)
-  f = OpenFileCREAT(fileName)
-  CloseFile(f)
-  f = OpenFileWR(fileName)
-  WriteFileString(f, "EOS M6 Mark II: EnableBootDisk() was called.\n")
-  CloseFile(f)
 end sub
